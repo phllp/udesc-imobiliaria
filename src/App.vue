b62de97df6accac5979596198ba2badbef947517
phllp <beigerfelipe@gmail.com>
2023-07-05 10:30:21 -0300

    style: add min-height to main component

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 <script>
 import PageHeader from '@/components/PageHeader.vue'
 import PageFooter from '@/components/PageFooter.vue';
+import './styles/styles.css'
 
 export default {
   name: 'App',
